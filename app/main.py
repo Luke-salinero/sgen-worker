8ab8b4af3aa68dd740580374043d02c52c278f2f
Brian Klemm <brian@bigsigma.tech>
2026-02-13 13:36:09 -0800

    Add status router to main

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import threading
 from app.worker import Worker
 from fastapi import FastAPI
 from app.results import router as results_router
+from app.status import router as job_status_router
 
 def create_app() -> FastAPI:
     app = FastAPI(
@@ -11,6 +12,7 @@ def create_app() -> FastAPI:
     )
 
     app.include_router(results_router)
+    app.include_router(job_status_router)
 
     @app.on_event("startup")
     def start_worker():
